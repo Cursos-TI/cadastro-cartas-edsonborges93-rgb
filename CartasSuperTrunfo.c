71ab1f886772b5c789120bd1fcef94bc2300a3f3
edsonborges93-rgb <edson.borges93@hotmail.com>
2025-10-16 01:14:36 +0000

    Impressão das Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,6 +21,8 @@ int numero_de_pontos_turisticos;
 
   // Área para entrada de dados
 
+printf("Digite os dados da Carta1\n");
+
 printf("estado : \n");
 scanf("%c", estado);
 
@@ -45,8 +47,8 @@ scanf("%s", &numero_de_pontos_turisticos);
   // Área para exibição dos dados da cidade
 
   // CARTA 02
-
-  char estado1[1];
+ 
+char estado1[1];
 char codigo_da_carta1[3];
 char nome_da_cidade1[50];
 int populacao1;
@@ -59,6 +61,8 @@ int numero_de_pontos_turisticos1;
   // Área para entrada de dados
 
   // Carta 02 // Cidade 02
+
+printf("\nDigite os dados da Carta2\n");
 
 printf("estado : \n");
 scanf(" %c", estado1);
@@ -81,7 +85,30 @@ scanf("%f", &pib3);
 printf("numero de pontos turisticos :\n");
 scanf("%s", &numero_de_pontos_turisticos1);
 
+ // Área para exibição dos dados da cidade
 
+    printf("\n--- DADOS DA CARTA 1 ---\n");
+
+    printf("Estado: %c\n", estado);
+    printf("Código da carta: %s\n", codigo_da_carta);
+    printf("Cidade: %s\n", nome_da_cidade);
+    printf("População: %d habitantes\n", populacao);
+    printf("Área: %f km²\n", area_em_km²);
+    printf("PIB: %f bilhões\n", pib1);
+    printf("Pontos turísticos: %s\n", &numero_de_pontos_turisticos);
+
+
+     // Área para exibição dos dados da cidade
+
+    printf("\n--- DADOS DA CARTA 2 ---\n");
+
+printf("Estado: %c\n", estado1);
+printf("Código da carta: %s\n", codigo_da_carta1);
+printf("Cidade: %s\n", nome_da_cidade1);
+printf("População: %d habitantes\n", populacao1);
+printf("Área: %f km²\n", area_em_km²1);
+printf("PIB: %f bilhões\n", pib3);
+printf("Pontos turísticos: %s\n", &numero_de_pontos_turisticos1);
 
 return 0;
 } 
